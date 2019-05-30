f26c7fd106cd99a1aae993ab06d5b50b42d85088
Carl Allan Vela <carlallanvela@gmail.com>
2019-05-30 22:06:49 +0800

    [CVELA] Added  headers.
    Added headers.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -8,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class PostCreateComponent {
   enteredValue = '';
   newPost = 'NO CONTENT';
+  // newPost2 = '';
+  // newPost3 = '';
 
   onAddPost() {
     this.newPost = this.enteredValue;
-
   }
+
+  // onAddPost2() {
+  //   this.newPost2 = 'THIS IS A NEW POST 2';
+  // }
+
+  // onAddPost3(postInput : HTMLTextAreaElement) {
+  //   this.newPost3 = postInput.value;
+  // }
 }
