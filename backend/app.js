fe8a9eab7873115263f44439ef290b835ea90e8e
Carl Allan Vela <carlallanvela@gmail.com>
2019-06-03 20:47:20 +0800

    [CVELA] Adding fetch.
    Adding fetch.

--- backend/app.js
+++ backend/app.js
@@ -5,32 +5,21 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-// mongoose.connect(
-//   'mongodb+srv://admin2:WBAkJdfp2lcyejnq@cluster0-ojzmt.mongodb.net/',
-//   {
-//     keepAlive: true,
-//     socketTimeoutMS: 30000,
-//     dbName: 'node-angular',
-//     useNewUrlParser: true
-//   }
-// )
-// .then(() => {
-//   console.log('Connected to database!');
-// })
-// .catch((err) => {
-//   console.log('Connection failed!', err.message);
-// });
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:test@cluster0-ojzmt.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.connect(
+  'mongodb+srv://admin:dummy@cluster1-ojzmt.mongodb.net/',
+  {
+    keepAlive: true,
+    socketTimeoutMS: 30000,
+    dbName: 'node-angular',
+    useNewUrlParser: true
+  }
+)
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch((err) => {
+  console.log('Connection failed!', err.message);
 });
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -61,22 +50,13 @@ app.post('/api/posts', (req, res, nex) => {
 
 // Contoller
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '12345',
-      title: 'First server-side post',
-      content: 'This is coming from the server.'
-    },
-    {
-      id: '23456',
-      title: 'Second server-side post',
-      content: 'This is coming from the server.'
-    }
-  ];
-  res.status(200).json({
-    message: 'Post fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Post fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
